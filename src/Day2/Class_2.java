819bc5151c715e0c0319af02db47de8e9883272b
MannKandoi09 <mannkandoi2005@gmail.com>
2026-09-13 14:45:17 +0530

    Class Updated

--- src/Day2/Class_2.java
+++ src/Day2/Class_2.java
@@ -1,4 +1,22 @@
 package Day2;
 
 public class Class_2 {
+    int id;
+    String name;
+    int marks;
+
+   void display(){
+       System.out.println("ID: " + id);
+       System.out.println("Name: " + name);
+       System.out.println("Marks: " +marks);
+   }
+
+    public static void main(String[] args) {
+        Class_2 c = new Class_2();
+        c.id = 101;
+        c.name = "Mann";
+        c.marks = 100;
+        c.display();
+
+    }
 }
