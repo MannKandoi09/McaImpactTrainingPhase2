2227da3e5c1735e04b9bfeef806d9d226b187d2a
MannKandoi09 <mannkandoi2005@gmail.com>
2026-09-13 14:20:55 +0530

    Updates class

--- src/Day2/Class_1.java
+++ src/Day2/Class_1.java
@@ -1,4 +1,11 @@
 package Day2;
 
 public class Class_1 {
+    public static void main(String[] args) {
+        String a = new String("Hello");
+        String b = new String("Hello");
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
 }
